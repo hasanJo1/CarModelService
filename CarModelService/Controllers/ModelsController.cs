dbd95b4bacc2ae391323b4c22472758ec99eee74
Hasan Al-Snaid <Hasan.Snaid@info-seed.com>
2024-04-16 17:42:53 +0300

    Create ModelsController and delete the defult controler

---
+++ CarModelService/Controllers/ModelsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarModelService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ModelsController : ControllerBase
+    {
+
+        private readonly ILogger<ModelsController> _logger;
+
+        public ModelsController(ILogger<ModelsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<string>>> GetModels(int modelyear, string make)
+        {
+
+            return Ok();
+        }
+    }
+}
